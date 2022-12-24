98efa85c0dbccb2b93813103f975f6747c4add62
Fabricio Tiago Arantes <88052209+Ujs74wiop6@users.noreply.github.com>
2022-12-24 00:31:02 -0300

    Update 24/12/2022

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-//Function Function that calculates my age, as the years go by...
-var idadeX;
+//Function Function that calculates my age, as the years go by...  
+// getFullYear() 
+function idadeX(){
+    var dt = new Date();
+    document.write(dt.getFullYear() - 2003);    
+}

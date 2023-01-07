803a6bfe5f529a70fab18a434cef9656be141424
Fabricio Tiago Arantes <88052209+Ujs74wiop6@users.noreply.github.com>
2023-01-06 22:20:35 -0300

    Update 06/01/2023

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-//Function Function that calculates my age, as the years go by...  
-// getFullYear() 
-function idadeX(){
-    var dt = new Date();
-    document.write(dt.getFullYear() - 2003);    
+function idadex(){ 
+    var dob = new Date("09/09/2003");
+    //calculate month difference from current date in time  
+    var month_diff = Date.now() - dob.getTime()
+
+    //convert the calculated difference in date format  
+    var age_dt = new Date(month_diff);
+
+    //extract year from date      
+    var year = age_dt.getUTCFullYear();
+
+    //now calculate the age of the user  
+    var age = Math.abs(year - 1970);
+
+    //display the calculated age  
+    //document.write(age);
+    console.log(age);
 }
+console.log(idadex());

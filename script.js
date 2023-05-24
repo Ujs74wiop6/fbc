fedc1bb3cb181247720917f79f16ae7a8b04149d
Fabricio Tiago Arantes <88052209+Ujs74wiop6@users.noreply.github.com>
2023-05-23 22:08:26 -0300

    Atualizações 23/05/2023

--- script.js
+++ script.js
@@ -1,19 +1,9 @@
 function idadex(){ 
     var dob = new Date("09/09/2003");
-    //calculate month difference from current date in time  
     var month_diff = Date.now() - dob.getTime()
-
-    //convert the calculated difference in date format  
     var age_dt = new Date(month_diff);
-
-    //extract year from date      
     var year = age_dt.getUTCFullYear();
-
-    //now calculate the age of the user  
     var age = Math.abs(year - 1970);
-
-    //display the calculated age  
-    //document.write(age);
     console.log(age);
 }
 console.log(idadex());
